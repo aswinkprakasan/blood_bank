5ffa6a2dca9534023f2ecca4248919b6bbc84355
groot <aswinachuo2001@gmail.com>
2022-07-25 04:50:09 +0530

    ldonmod

--- main_framework/backend/_last_donation.php
+++ main_framework/backend/_last_donation.php
@@ -5,11 +5,20 @@ include ('config.php');
 $last_don=$_POST['last_donation'];
 $email=$_SESSION["mail"];
    
-$sql="INSERT INTO last_donation_table(email,month,last_don_date) VALUES ('$email', '0','$last_don')";
+// $sql="INSERT INTO last_donation_table (email,days,last_don_date) VALUES ('$email', '0','$last_don')";
+// $result=mysqli_query($conn,$sql);
+
+$sql1="SELECT DATEDIFF(now(),`last_don_date`) AS dated FROM last_donation_table WHERE email=$email";
+$result=mysqli_query($conn,$sql1);
+echo $result;
+$data=mysqli_fetch_row($result);
+
+$sql="UPDATE `last_donation_table` SET `days` = '$data[0]' WHERE `last_donation_table`.`email` = '$email'";
 $result=mysqli_query($conn,$sql);
+
     
-$sql = "UPDATE `last_donation_table` SET `last_don_date` = '$last_don' WHERE `last_donation_table`.`email` = '$email'";
-$result=mysqli_query($conn,$sql); 
+// $sql = "UPDATE `last_donation_table` SET `last_don_date` = '$last_don' WHERE `last_donation_table`.`email` = '$email'";
+// $result=mysqli_query($conn,$sql); 
 
 header("Location:../last_donation.php");
 
